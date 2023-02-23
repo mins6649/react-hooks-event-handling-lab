63351430ab7f209da5ce08f304de8451162b5807
minyoungshin <mins6649@gmail.com>
2023-02-22 19:34:38 -0500

    events in React

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,13 @@
 // Code Keypad Component Here
 
 function Keypad (){
+    function changePassword(event){
+        console.log("Entering password...")
+    }
     return (
-        <div></div>
+        <div>
+            <input type="password" onChange={changePassword}></input>
+        </div>
     )
 }
 
